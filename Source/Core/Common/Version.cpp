e9dda1e687cf3142d3bf7679e290be1ba1a8cd79
Jas Laferriere <Fizzi36@gmail.com>
2026-05-22 15:50:37 -0400

    release(netplay): v4.0.0-mainline-beta.17

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -20,7 +20,7 @@ namespace Common
 #endif
 
 #ifndef IS_PLAYBACK
-#define SLIPPI_REV_STR "4.0.0-mainline-beta.16"  // netplay version
+#define SLIPPI_REV_STR "4.0.0-mainline-beta.17"  // netplay version
 #else
 #define SLIPPI_REV_STR "3.2.0"  // playback version
 #endif
